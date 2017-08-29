65c6e5b54581364ec5ef910bd36dec0295b1b102
Michael Joyner <joyner.michael@gmail.com>
2017-08-29 10:27:44 +0800

    remove invisible unicode character that was causing shit

--- resources/views/front/home/did-you-know.blade.php
+++ resources/views/front/home/did-you-know.blade.php
@@ -9,11 +9,11 @@
             </li>
             <li class="tc mb3 mb4-ns">
                 <p class="strong-type f4 f3-ns mv2 colour-p">31%</p>
-                <p class="body-type colour-p mv2">Decrease in the use of labour augmentation </p>
+                <p class="body-type colour-p mv2">Decrease in the use of labour augmentation</p>
             </li>
             <li class="tc mb3 mb4-ns">
                 <p class="strong-type f4 f3-ns mv2 colour-p">28%</p>
-                <p class="body-type colour-p mv2">Decrease in the risk of C-section </p>
+                <p class="body-type colour-p mv2">Decrease in the risk of C-section</p>
             </li>
             <li class="tc mb3 mb4-ns">
                 <p class="strong-type f4 f3-ns mv2 colour-p">14%</p>
@@ -21,11 +21,11 @@
             </li>
             <li class="tc mb3 mb4-ns">
                 <p class="strong-type f4 f3-ns mv2 colour-p">12%</p>
-                <p class="body-type colour-p mv2">Increase in the likelihood of a spontaneous vaginal birth </p>
+                <p class="body-type colour-p mv2">Increase in the likelihood of a spontaneous vaginal birth</p>
             </li>
             <li class="tc mb3 mb4-ns">
                 <p class="strong-type f4 f3-ns mv2 colour-p">9%</p>
-                <p class="body-type colour-p mv2">Decrease in the use of any medications for pain relief </p>
+                <p class="body-type colour-p mv2">Decrease in the use of any medications for pain relief</p>
             </li>
         </ul>
         <p class="tr pr3 pr5-ns f5 f5-ns colour-p i body-type mv5">&mdash; Hodnett et al, 2012</p>
