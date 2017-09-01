ca1e5cd7e30552551473691b58ce76d4ae6aef05
Michael Joyner <joyner.michael@gmail.com>
2017-09-01 08:59:47 +0800

    add backups

--- config/filesystems.php
+++ config/filesystems.php
@@ -67,10 +67,10 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => env('AWS_KEY'),
-            'secret' => env('AWS_SECRET'),
-            'region' => env('AWS_REGION'),
-            'bucket' => env('AWS_BUCKET'),
+            'key' => env('AMAZON_S3_KEY'),
+            'secret' => env('AMAZON_S3_SECRET'),
+            'region' => env('AMAZON_S3_REGION'),
+            'bucket' => env('AMAZON_S3_BUCKET'),
         ],
 
     ],
