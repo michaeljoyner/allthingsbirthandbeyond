8430263c52040681806a8e3ef5c6379c536de3e7
Michael Joyner <joyner.michael@gmail.com>
2020-04-11 10:03:21 +0800

    hover on affiliates

--- resources/views/front/home/affiliates.blade.php
+++ resources/views/front/home/affiliates.blade.php
@@ -6,7 +6,7 @@
             <p class="type-h2 text-purple text-center">{{ $affiliate_group['title'] }}</p>
             <div class="flex justify-center mt-6">
                 @foreach($affiliate_group['items'] as $affiliate)
-                <div class="w-28 h-28 md:w-48 md:h-48 mx-1 md:mx-6 border-2 border-navy">
+                <div class="w-28 h-28 md:w-48 md:h-48 mx-1 md:mx-6 border-4 border-navy @if($affiliate['link'])hover:border-teal-mid @endif">
                     @if($affiliate['link'])
                     <a href="{{ $affiliate['link'] }}" target="_blank" rel="noopener">
                     @endif
