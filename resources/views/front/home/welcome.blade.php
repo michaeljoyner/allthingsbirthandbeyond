88db880bfe2f3a91a0d5dc7ae6ffb105e64dfaee
Michael Joyner <joyner.michael@gmail.com>
2020-04-14 09:26:56 +0800

    fix typo and affiliate sizing on mobile

--- resources/views/front/home/welcome.blade.php
+++ resources/views/front/home/welcome.blade.php
@@ -1,4 +1,4 @@
 <div class="px-6 pb-20 md:pt-20 -mt-20 md:mt-0">
     <p class="type-h1 text-center text-purple">Welcome</p>
-    <p class="max-w-2xl mx-auto my-12 type-b1 text-center">When a woman feel empowered in her birth experience, and has continuity of care during her transition to motherhood, this has a positive impact on her health and well-being - as well as on her relationship with her baby.</p>
+    <p class="max-w-2xl mx-auto my-12 type-b1 text-center">When a woman feels empowered in her birth experience, and has continuity of care during her transition to motherhood, this has a positive impact on her health and well-being - as well as on her relationship with her baby.</p>
 </div>
